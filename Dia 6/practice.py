81bf557a0ec751aad2e98c60004d6fae44677e6a
Charlie-Tc <quispecaleb77@gmail.com>
2023-07-09 09:18:38 -0500

    The file "practice.py" has finished.

--- Dia 6/practice.py
+++ Dia 6/practice.py
@@ -11,15 +11,10 @@ def choose_optios():
 
     print(f'the folder \033[1mRecipes\033[0m is in {Path.cwd()}\n')
 
-    print(''' you have \033[1m6\033[0m options to choose:
-    [1] Read Recipes
-    [2] Create Recipes
-    [3] Create Content
-    [4] Create Category
-    [5] Delete Recipes
-    [6] Delete Category
-    [7] Delete content
-    [8] End Program
+    print(''' you have \033[1m8\033[0m options to choose:
+    1️⃣ Read Recipes    |   2️⃣ Create Recipes  |   3️⃣ Create Content  |   4️⃣ Create Category
+    -----------------------------------------------------------------------------------------
+    5️⃣ Delete Recipes  |   6️⃣ Delete Category |   7️⃣ Delete content  |   8️⃣ End Program
     ''')
 
 
@@ -29,10 +24,12 @@ def choose_category():
 
 
     print('You have the following categories to choose from')
+    print('\n')
     for category in folders:
-        print(f'Category of {category}')
+        print(f'➡ {category}')
 
     while True:
+        print('\n')
         insert = input('Choose a Category: ').title()
         if insert in folders:
             return base / insert
@@ -46,6 +43,7 @@ def display_recipes(selected_route):
     recipes = False
 
     if selected_route.is_dir():
+        print('\t👇')
         for file in selected_route.iterdir():
             if file.is_file():
                 print(file.name)
@@ -60,11 +58,13 @@ def choose_recipe():
     display_recipes(selected_route)
 
     while True:
+        print('*' * 50)
         recipe_name = input('Choose a Recipe: ').lower() + '.txt'
         recipe_path = selected_route / recipe_name
 
         if recipe_path.is_file():
             print(f"You selected the recipe: {recipe_name}")
+            print('*' * 50)
             break
         else:
             print("Invalid recipe. Please choose a valid recipe.")
@@ -76,16 +76,20 @@ def display_content():
     recipe_choose = choose_recipe()
     options = ['open','exit']
 
-    print('you have 2 options to choose: OPEN and EXIT')
-    user_choose = input('Choose a option: ').lower()
-    if user_choose == 'exit' and user_choose in options:
-        print(f'you chosen exit')
-    elif user_choose == 'open' and user_choose in options:
-        open_file = open(recipe_choose, 'r', encoding="utf-8")
-        content_display = open_file.read()
-        print(content_display)
-    else:
-        print('Choose a valid option')
+
+    while True:
+        print('you have 2 options to choose: OPEN and EXIT')
+        user_choose = input('Choose a option: ').lower()
+        if user_choose == 'exit' and user_choose in options:
+            print(f'you chosen exit')
+            break
+        elif user_choose == 'open' and user_choose in options:
+            open_file = open(recipe_choose, 'r', encoding="latin-1")
+            content_display = open_file.read()
+            print(content_display)
+            break
+        else:
+            print('Choose a valid option')
 
 
 def delete_category():
@@ -151,36 +155,55 @@ def delete_content():
 
 
 def finaliting_program():
-    game_over = True
     print('Program Finished')
-    return True
+
+def return_home():
+    while True:
+        print('*' * 50)
+        user = input('Press the number 1 to return: ')
+        if user == '1':
+            break
+        else:
+            print('⚠ Invalid comand ⚠')
 
 
-while True:
-    choose_optios()
+'''def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')'''
+
+def game():
+    while True:
+        choose_optios()
+        enter_option = input('Enter a option: ')
+
+        if enter_option == '1':
+            display_content()
+            return_home()
+        elif enter_option == '2':
+            create_recipes()
+            return_home()
+        elif enter_option == '3':
+            create_content()
+            return_home()
+        elif enter_option == '4':
+            create_category()
+            return_home()
+        elif enter_option == '5':
+            delete_recipes()
+            return_home()
+        elif enter_option == '6':
+            delete_category()
+            return_home()
+        elif enter_option == '7':
+            delete_content()
+            return_home()
+        elif enter_option == '8':
+            finaliting_program()
+            break
+        else:
+            print('Enter a option valid')
 
 
-    enter_option = int(input('Enter a option: '))
-
-    if enter_option == 1:
-        display_content()
-    elif enter_option == 2:
-        create_recipes()
-    elif enter_option == 3:
-        create_content()
-    elif enter_option == 4:
-        create_category()
-    elif enter_option == 5:
-        delete_recipes()
-    elif enter_option == 6:
-        delete_category()
-    elif enter_option == 7:
-        delete_content()
-    elif enter_option == 8:
-        finaliting_program()
-        break
-    else:
-        print('Enter a option valid')
+game()
 
 
 
